ef92d61a1366cf3338c3eda7bd01d32483318f6d
Sviatoslav Sobakin <slavasobb@gmail.com>
2020-08-14 17:15:45 +0300

    move pivot fix

--- Assets/First.cs
+++ Assets/First.cs
@@ -17,22 +17,25 @@ public class First : MonoBehaviour
     {
         facingRight = !facingRight;
 
-        this.m_SpriteRenderer.flipX = !facingRight;
+        Vector3 scale = this.transform.localScale;
+        scale.x = scale.x * -1;
+
+        transform.localScale = scale;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        m_Animator = gameObject.GetComponent<Animator>();
-        m_SpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        m_Animator = gameObject.GetComponentInChildren<Animator>();
+        m_SpriteRenderer = gameObject.GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float move = Input.GetAxis("Horizontal");
+        this.move = Input.GetAxis("Horizontal");
 
-        if (move == 0)
+        if (this.move == 0)
         {
             m_Animator.SetBool("isStay", true);
         }
