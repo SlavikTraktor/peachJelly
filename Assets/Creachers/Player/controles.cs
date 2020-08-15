b89ffe761f094178c1bb3834a6d266aa1b0cecdd
A1truf <thepunisher6661@gmail.com>
2020-08-15 17:51:12 +0300

    Ball

---
+++ Assets/Creachers/Player/controles.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controles : MonoBehaviour
+{
+    public float horizontalSpeed = 0.2f;
+    float speedX;
+    public float verticalSpeed = 0.1f;
+    float speedY;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            speedX = -horizontalSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            speedX = horizontalSpeed;
+        }
+            transform.Translate(speedX, 0, 0);
+        speedX = 0;
+        if (Input.GetKey(KeyCode.S))
+        {
+            speedY = -verticalSpeed;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            speedY = verticalSpeed;
+        }
+
+        transform.Translate(0, speedY, 0);
+        speedY = 0;
+    }
+}
