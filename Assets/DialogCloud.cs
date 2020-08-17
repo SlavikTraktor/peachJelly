440048c6e89ec40eeae3a322fa1623fc9d42b9a9
A1truf <thepunisher6661@gmail.com>
2020-08-17 17:29:03 +0300

    NPC

---
+++ Assets/DialogCloud.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogCloud : MonoBehaviour
+{
+    public Animator[] clouds;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (Animator anim in clouds) 
+            {
+anim.SetTrigger("New Trigger");
+             }
+        }
+    }
+      public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (Animator anim in clouds) 
+            {
+anim.SetTrigger("New Trigger");
+             }
+        }
+    }
+}
